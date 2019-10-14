db80bee176a547bff25be429cad988f978082b21
Avijn <38077967+Avijn@users.noreply.github.com>
2019-10-14 10:50:14 +0200

    Photo page and photo function

---
+++ functions.php
@@ -0,0 +1,97 @@
+<?php
+
+function imports() {
+    ?>
+    <title>TU Schoonebeek</title>
+    <meta charset="UTF-8">
+    <link type="text/css" rel="stylesheet" href="css/style.css"/>
+    <?php
+}
+
+function headerBar() {
+    ?>
+    <div id="header">
+        <a id="logo" href="index.php">
+            <img id="logoImg" src="img/Logo_Transparant.png" alt="logo TU Schoonebeek">
+        </a>
+        <h2>Schoonebeek</h2>
+    </div>
+    <?php
+}
+
+function navBar($fileName) {
+    //Verander hier de namen
+    $page1 = "studiePage.php";
+    $page2 = "photoPage.php";
+    $page3 = "contactPage.php";
+    $page4 = "index.php";
+    $name1 = "Opleidingen";
+    $name2 = "Foto's";
+    $name3 = "Contact";
+
+    //Geeft de juiste navigatie balk aan de juiste pagina mee (reden is blauwe streep (id="usingPage") en de hoofdpagina)
+    if($fileName == $page2) {
+        ?>
+        <div id="navBarWhithoutRss">
+			<ul>
+				<li><a href="<?php echo $page1 ?>"><?php echo $name1 ?></a></li>
+				<li id="usingPage"><a  href="<?php echo $page2 ?>"><?php echo $name2 ?></a></li>
+				<li><a href="<?php echo $page3 ?>"><?php echo $name3 ?></a></li>
+			</ul>
+		</div>
+		<?php
+    } elseif ($fileName == $page1) {
+        ?>
+        <div id="navBarWhithoutRss">
+            <ul>
+                <li id="usingPage"><a href="<?php echo $page1 ?>"><?php echo $name1 ?></a></li>
+                <li><a  href="<?php echo $page2 ?>"><?php echo $name2 ?></a></li>
+                <li><a href="<?php echo $page3 ?>"><?php echo $name3 ?></a></li>
+            </ul>
+        </div>
+        <?php
+    } elseif ($fileName == $page3) {
+        ?>
+        <div id="navBarWhithoutRss">
+            <ul>
+                <li><a href="<?php echo $page1 ?>"><?php echo $name1 ?></a></li>
+                <li><a  href="<?php echo $page2 ?>"><?php echo $name2 ?></a></li>
+                <li id="usingPage"><a href="<?php echo $page3 ?>"><?php echo $name3 ?></a></li>
+            </ul>
+        </div>
+        <?php
+    } elseif ($fileName == $page4) {
+        ?>
+        <div id="navBar">
+            <ul>
+                <li><a href="<?php echo $page1 ?>"><?php echo $name1 ?></a></li>
+                <li><a  href="<?php echo $page2 ?>"><?php echo $name2 ?></a></li>
+                <li><a href="<?php echo $page3 ?>"><?php echo $name3 ?></a></li>
+            </ul>
+        </div>
+        <?php
+    } else {
+        ?>
+        <div id="navBarWhithoutRss">
+            <ul>
+                <li><a href="<?php echo $page1 ?>"><?php echo $name1 ?></a></li>
+                <li><a  href="<?php echo $page2 ?>"><?php echo $name2 ?></a></li>
+                <li><a href="<?php echo $page3 ?>"><?php echo $name3 ?></a></li>
+            </ul>
+        </div>
+        <?php
+    }
+}
+
+function footer() {
+    ?>
+    <div id="footer">
+        <!--Laat copyright zien van 2019 tot het huidig jaar-->
+        <?php echo "<p>&copy;" . "2019-" . date("Y") . " Technisch universiteit Schoonebeek </p>" ?>
+    </div>
+    <?php
+}
+ function test($fileName) {
+
+ }
+?>
---
+++ photo.functions.php
@@ -0,0 +1,26 @@
+<?php       
+    function photo() {
+        $imgarray = array();
+        $path = "photos";                                    //Path naar img
+        $file = scandir($path);                                 //scant directory zet alle files in een array
+        if ($file) {
+
+            foreach ($file as $key => $img) {                       //Deze foreach vangt . en .. af krijgt unix timestamps van de files
+                $extensioncheck = pathinfo($img);                   //Checked file extension van de img
+
+                if ($extensioncheck['extension'] == "gif" || $extensioncheck['extension'] == "png" || $extensioncheck['extension'] == "jpg" || $extensioncheck['extension'] == "jpeg") {
+
+                    $time = filectime($path."\\" . $img);        //Filectime krijgt de unix timestamp terug van het moment dat de file wordt OPGESLAGEN
+                    $imgarray[$img] = array($time);                 //Files worden in een nieuwe array gezet de key van elk object is de bestandsnaam de value de unix timestamp               
+                } else {
+                    
+                }
+            }
+            arsort($imgarray);                                      //Sorteert op timestamp zodat nieuwste boven komt
+
+            foreach ($imgarray as $key => $value) {
+                print("<img src=" . $path . "\\" . $key . ">");     //print de key (img file) als <img>
+            }
+        }
+    }
+?>
