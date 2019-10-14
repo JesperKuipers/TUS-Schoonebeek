99b7a92ab10fc444b9c707a855ae0f05ed68f2f5
ywielage <32356416+ywielage@users.noreply.github.com>
2019-10-14 10:30:58 +0200

    Update RssTest.php
    Code met alleen titel

--- RssTest.php
+++ RssTest.php
@@ -1,18 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <!--Bevat functions.php voor de lay-out, zorgt dat alle functies van functions.php hier gebruikt kan worden-->
-    <?php require_once "functions.php" ?>
-    <!--Importeer de basis informatie-->
-    <?php imports() ?>
 </head>
 <body>
-<!--Importeren van de header-->
-<?php headerBar() ?>
-<div id="contentAndRssContainer">
-    <!--Importeren van de Navigatie balk (aan de functie wordt de huidige bestand naam mee gegeven-->
-    <?php navBar(basename($_SERVER['PHP_SELF'])) ?>
-    <div id="rssFeed">
+
         <?php
         $rss = new DOMDocument();
         $rss->load('https://tweakers.net/feeds/nieuws.xml');
@@ -20,9 +11,7 @@
         foreach ($rss->getElementsByTagName('item') as $node) {
             $item = array (
                 'title' => $node->getElementsByTagName('title')->item(0)->nodeValue,
-                'desc' => $node->getElementsByTagName('description')->item(0)->nodeValue,
                 'link' => $node->getElementsByTagName('link')->item(0)->nodeValue,
-                'date' => $node->getElementsByTagName('pubDate')->item(0)->nodeValue,
             );
             array_push($feed, $item);
         }
@@ -30,40 +19,9 @@
         for($x=0;$x<$limit;$x++) {
             $title = str_replace(' & ', ' &amp; ', $feed[$x]['title']);
             $link = $feed[$x]['link'];
-            $description = $feed[$x]['desc'];
-            $date = date('l F d, Y', strtotime($feed[$x]['date']));
-            echo '<p><strong><a href="'.$link.'" title="'.$title.'">'.$title.'</a></strong><br />';
-            echo '<small><em>Posted on '.$date.'</em></small></p>';
-            echo '<p>'.$description.'</p>';
+            echo '<p style="color:red;"><a style="font-size:11px;" href="'.$link.'" title="'.$title.'">'.$title.'</a><br />';
         }
         ?>
-    </div>
-    <div id="contentContainer">
-        <div class="imgOrTextContainer">
-            <h1>Studeren aan technische universiteit Schoonebeek</h1>
-            <p>
-                Kies jij ervoor om te studeren op Technische Universiteit Schoonebeek?
-                Dan begin je aan de tijd van je leven! Op deze universiteit krijg je de kans om jouw talenten te ontdekken en ontwikkelen.
-                Op hetzelfde moment krijg je de kans om vol van het studentenleven genieten.
-            </p>
-            <p><a href="https://duo.nl/particulier/studiefinanciering/index.jsp " class="button" target="_blank">Klik hier voor meer informatie.</a>
-                Studeren kost vaak veel geld en omdat veel van onze studenten van over de hele wereld komen
-                (Europa, Azië en zelfs uit Zuid-Amerika), zijn onze opleidingen betaalbaar en worden veel kosten vergoed door DUO.
-            </p>
-        </div>
-        <div class="imgOrTextContainer">
-            <h1>Oorsprong </h1>
-            <p>
-                De Technische Universiteit te Schoonebeek heeft zijn ontstaan te danken aan zwerver Floris.
-                Onder de inwoners van Schoonebeek is zwerver Floris erg bekend,
-                niet alleen omdat hij onze Technische Universiteit Schoonebeek heeft gesticht, maar ook om zijn karakter en persoonlijkheid.
-                Zwerver Floris is jarenlang afhankelijk geweest van zijn dorpsgenoten en heeft de hulp van zijn naasten vervolgens
-                terugbetaald door met het idee van een TU te komen. Nadat hij op dit idee was gekomen heeft hij het uitgewerkt en op gang gebracht. Zo is de Technische Universiteit te Schoonebeek ontstaan.
-            </p>
-        </div>
-        <!--Importeren van de Footer-->
-        <?php footer() ?>
-    </div>
-</div>
+
 </body>
 </html>
