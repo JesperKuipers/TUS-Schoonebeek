200197ac5a3457c78e36808bc39fdf9d8bea0313
JesperKuipers <jesperkuipers@live.nl>
2019-10-14 10:04:21 +0200

    first test

--- RssTest.php
+++ RssTest.php
@@ -1,55 +1,32 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-    
+
 </head>
 <body>
-    <div id="TweakersDiv">
-<div id="tnetHeadlines">Fout</div>
-<script>
-
-    var TNET = (function()
-    {
-        var updateTimer = 0; // seconds, use 0 if you don't want auto refresh
-        var numHeadlines = 10;
-
-        var writeHeadlines = function(headlines)
-        {
-            var tnetDiv = document.getElementById('tnetHeadlines');
-            if (headlines.length && tnetDiv)
-            {
-                for (var i = 0, headline, html = []; i < numHeadlines && (headline = headlines[i]); i++)
-                {
-                    html.push('<a href="' + headline.link + '">' + headline.title + '<\/a>');
-                }
-
-                tnetDiv.innerHTML = html.join('<br>');
-            }
-
-            if (updateTimer)
-                setTimeout(update, updateTimer * 1000);
-        };
-
-        var script;
-        var update = function()
-        {
-            if (script)
-                script.parentNode.removeChild(script);
-
-            var f = document.getElementsByTagName('script')[0];
-
-            script = document.createElement('script'); script.async = true;
-            script.src = 'http://tweakers.net/feeds/nieuws.json?jsonp=TNET.write';
-            f.parentNode.insertBefore(script, f);
-        }
-
-        update();
-
-        return {write:writeHeadlines};
-        
-    })();
-
-</script> 
-    </div>
+<?php
+$rss = new DOMDocument();
+$rss->load('https://tweakers.net/feeds/va.xml');
+$feed = array();
+foreach ($rss->getElementsByTagName('item') as $node) {
+    $item = array (
+        'title' => $node->getElementsByTagName('title')->item(0)->nodeValue,
+        'desc' => $node->getElementsByTagName('description')->item(0)->nodeValue,
+        'link' => $node->getElementsByTagName('link')->item(0)->nodeValue,
+        'date' => $node->getElementsByTagName('pubDate')->item(0)->nodeValue,
+    );
+    array_push($feed, $item);
+}
+$limit = 5;
+for($x=0;$x<$limit;$x++) {
+    $title = str_replace(' & ', ' &amp; ', $feed[$x]['title']);
+    $link = $feed[$x]['link'];
+    $description = $feed[$x]['desc'];
+    $date = date('l F d, Y', strtotime($feed[$x]['date']));
+    echo '<p><strong><a href="'.$link.'" title="'.$title.'">'.$title.'</a></strong><br />';
+    echo '<small><em>Posted on '.$date.'</em></small></p>';
+    echo '<p>'.$description.'</p>';
+}
+?>
 </body>
 </html>
