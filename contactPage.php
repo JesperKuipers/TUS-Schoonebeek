1cd4b9d8a19daefebb0cefba22688dd63933aa9a
RobinGoeree2 <56107687+RobinGoeree2@users.noreply.github.com>
2019-10-14 12:24:16 +0200

    Comments

--- contactPage.php
+++ contactPage.php
@@ -44,8 +44,9 @@
             <?php
             $request = $_SERVER["REQUEST_METHOD"];
 
-            if ($request == "POST") {
+            if ($request == "POST") { // Bij het indrukken van de knop wordt de Request method gelijk aan Post
 
+                // Haalt de teksten op uit de inputvelden
                 $fName = $_POST["firstName"];
                 $lName = $_POST["lastName"];
                 $Mail = $_POST["email"];
@@ -55,26 +56,29 @@
 
                 function waarSchuwing()
                 {
-
+                    // Stukje javascript waarbij er een waarschuwing gegeven wordt aan de gebruiker in een alert box
                     echo '<script type="text/javascript">alert("U vergeet wat in te vullen");</script>';
-
                 }
 
+                // Controle of alle inputvelden zijn ingevuld
                 if (!empty($fName) && !empty($lName) && !empty($Mail) && !empty($Tel) && !empty($Message)) {
-                    $to = "test@test.nl";
-                    $subject = "Contact";
+                    $to = "test@test.nl"; // Test email address
+                    $subject = "Contact"; // Onderwerp in de mail
+                    // Het bericht met vaste tekst en variabele namen
                     $message =  "Beste Mr/Mevr, \n \r\n" .
                                 $Message . "\r\n \r\n" .
                                 "Mijn gegevens zijn: \r\n Mail: $Mail \r\n Tel: $Tel \r\n \r\n" .
                                 "Met vriendelijke groet, \r\n $fName $lName";
+                    // Meta data voor de mail
                     $headers =  "From: $Mail" . "\r\n" .
                                 "Reply-To: $Mail" . "\r\n" .
                                 "X-Mailer: PHP/" . phpversion();
-
+                    // Als de verzend knop is ingedrukt wordt wordt de mail functie geactiveerd
                     if ($request == "POST" && isset($submit)) {
                         mail($to, $subject, $message, $headers);
                     }
                 } else {
+                    // Geeft een alert box weer met een waarschuwing
                     waarSchuwing();
                 }
             }
