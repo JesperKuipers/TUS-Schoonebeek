e1f96e91e69b74b116649846f6b370201e98360a
JesperKuipers <jesperkuipers@live.nl>
2019-11-08 10:18:00 +0100

    ContactPage & StudiePage
    Fix mail functie V2.
    Merge studiePage.

--- contactPage.php
+++ contactPage.php
@@ -1,130 +1,95 @@
 <!DOCTYPE html>
 <html>
-	<head>
-        <!--Bevat functions.php voor de lay-out, zorgt dat alle functies van functions.php hier gebruikt kan worden-->
-        <?php require_once "functions.php" ?>
-        <!--Importeer de basis informatie-->
-        <?php imports() ?>
-        <link type="text/css" rel="stylesheet" href="css/contactPage.css"/>
-	</head>
-	<body>
-        <!--Importeren van de header-->
-        <?php headerBar() ?>
-        <div id="contentAndRssContainer">
-            <!--Importeren van de Navigatie balk (aan de functie wordt de huidige bestand naam mee gegeven-->
-            <?php navBar(basename($_SERVER['PHP_SELF'])) ?>
-            <div id="contentContainerFullWidth">
-                <div class="imgOrTextContainer">
-                    <div id="contactGrid">
-                        <div id="contactGridCell1">
-                            <img src="img/person_Icon.png" alt="Icoon van een persoon">
-                            <form method="POST">
-                                <input type="text" name="firstName" placeholder="Uw voornaam">
-                                <input type="text" name="lastName" placeholder="Uw achternaam">
-                        </div>
-                        <div id="contactGridCell2">
-                            <img src="img/email_Icon.png" alt="Icoon van een brief">
-                            <input type="text" name="email" class="email" placeholder="Uw emailadres">
-                        </div>
-                        <div id="contactGridCell3">
-                            <img src="img/phone2_Icon.png" alt="Icoon van een telefoon">
-                            <input type="text" name="tel" class="tel" placeholder="Uw telefoonnummer">
-                        </div>
-                        <div id="contactGridCell4">
-                            <img src="img/message_Icon.png" alt="Icoon van een bericht">
-                            <textarea name="message" placeholder="Bericht" class="textarea"></textarea>
-                            <input type="submit" name="submit" class="verzendKnop">
-                            </form>
-                        </div>
-                    </div>
+<head>
+    <!--Bevat functions.php voor de lay-out, zorgt dat alle functies van functions.php hier gebruikt kan worden-->
+    <?php require_once "functions.php" ?>
+    <!--Importeer de basis informatie-->
+    <?php imports() ?>
+    <link type="text/css" rel="stylesheet" href="css/contactPage.css"/>
+</head>
+<body>
+<!--Importeren van de header-->
+<?php headerBar() ?>
+<div id="contentAndRssContainer">
+    <!--Importeren van de Navigatie balk (aan de functie wordt de huidige bestand naam mee gegeven-->
+    <?php navBar(basename($_SERVER['PHP_SELF'])) ?>
+    <div id="contentContainerFullWidth">
+        <div class="imgOrTextContainer">
+            <div id="contactGrid">
+                <div id="contactGridCell1">
+                    <img src="img/person_Icon.png" alt="Icoon van een persoon">
+                    <form method="POST">
+                        <input type="text" name="firstName" placeholder="Uw voornaam">
+                        <input type="text" name="lastName" placeholder="Uw achternaam">
                 </div>
-                <!--Importeren van de Footer-->
-                <?php footer() ?>
+                <div id="contactGridCell2">
+                    <img src="img/email_Icon.png" alt="Icoon van een brief">
+                    <input type="email" name="email" class="email" placeholder="Uw emailadres">
+                </div>
+                <div id="contactGridCell3">
+                    <img src="img/phone2_Icon.png" alt="Icoon van een telefoon">
+                    <input type="number" name="tel" class="tel" placeholder="Uw telefoonnummer">
+                </div>
+                <div id="contactGridCell4">
+                    <img src="img/message_Icon.png" alt="Icoon van een bericht">
+                    <textarea name="message" placeholder="Bericht" class="textarea"></textarea>
+                    <input type="submit" name="submit" class="verzendKnop">
+                    </form>
+                </div>
             </div>
-            <?php
-            $request = $_SERVER["REQUEST_METHOD"];
+        </div>
+        <!--Importeren van de Footer-->
+        <?php footer() ?>
+    </div>
+    <?php
+    $request = $_SERVER["REQUEST_METHOD"];
+    if ($request == "POST") { // Bij het indrukken van de knop wordt de Request method gelijk aan Post
+        // Haalt de teksten op uit de inputvelden
+        $fName = $_POST["firstName"];
+        $lName = $_POST["lastName"];
+        $Mail = $_POST["email"];
+        $Tel = $_POST["tel"];
+        $Message = $_POST["message"];
+        $submit = $_POST["submit"];
+        function waarSchuwing()
+        {
+            // Stukje javascript waarbij er een waarschuwing gegeven wordt aan de gebruiker in een alert box
+            echo '<script type="text/javascript">alert("U vergeet wat in te vullen");</script>';
+        }
 
-            if ($request == "POST") {
+        function mailMislukt()
+        {
+            // Stukje javascript waarbij er een error gegeven wordt aan de gebruiker in een alert box
+            echo '<script type="text/javascript">alert("Het is niet gelukt om de mail te versturen");</script>';
+        }
 
-                $fName = $_POST["firstName"];
-                $lName = $_POST["lastName"];
-                $Mail = $_POST["email"];
-                $Tel = $_POST["tel"];
-                $Message = $_POST["message"];
-                $submit = $_POST["submit"];
-
-                function waarSchuwing()
-                {
-
-                    echo '<script type="text/javascript">alert("U vergeet wat in te vullen");</script>';
-
-                }
-
-                if (!empty($fName) && !empty($lName) && !empty($Mail) && !empty($Tel) && !empty($Message)) {
-                    $to = "test@test.nl";
-                    $subject = "Contact";
-                    $message =  "Beste Mr/Mevr, \n \r\n" .
-                                $Message . "\r\n \r\n" .
-                                "Mijn gegevens zijn: \r\n Mail: $Mail \r\n Tel: $Tel \r\n \r\n" .
-                                "Met vriendelijke groet, \r\n $fName $lName";
-                    $headers =  "From: $Mail" . "\r\n" .
-                                "Reply-To: $Mail" . "\r\n" .
-                                "X-Mailer: PHP/" . phpversion();
-
-                    if ($request == "POST" && isset($submit)) {
-                        mail($to, $subject, $message, $headers);
-                    }
+        // Controle of alle inputvelden zijn ingevuld
+        if (!empty($fName) && !empty($lName) && !empty($Mail) && !empty($Tel) && !empty($Message)) {
+            $to = "test@test.nl"; // Test email address
+            $subject = "Contact"; // Onderwerp in de mail
+            // Het bericht met vaste tekst en variabele namen
+            $message = "Beste Mr/Mevr, \n \r\n" .
+                $Message . "\r\n \r\n" .
+                "Mijn gegevens zijn: \r\n Mail: $Mail \r\n Tel: $Tel \r\n \r\n" .
+                "Met vriendelijke groet, \r\n $fName $lName";
+            // Meta data voor de mail
+            $headers = "From: $Mail" . "\r\n" .
+                "Reply-To: $Mail" . "\r\n" .
+                "X-Mailer: PHP/" . phpversion();
+            // Als de verzend knop is ingedrukt wordt wordt de mail functie geactiveerd
+            if ($request == "POST" && isset($submit)) {
+                if (strpos($Mail, "@") == true && strlen(strval($Tel)) == 10 && is_numeric($Tel)) {
+                    @mail($to, $subject, $message, $headers);
                 } else {
-                    waarSchuwing();
+                    mailMislukt();
                 }
             }
-            ?>
-        </div>
-    </body>
-
-<!DOCTYPE html>
-<html>
-	<head>
-        <!--Bevat functions.php voor de lay-out, zorgt dat alle functies van functions.php hier gebruikt kan worden-->
-        <?php require_once "functions.php" ?>
-        <!--Importeer de basis informatie-->
-        <?php imports() ?>
-        <link type="text/css" rel="stylesheet" href="css/contactPage.css"/>
-	</head>
-	<body>
-        <!--Importeren van de header-->
-        <?php headerBar() ?>
-        <div id="contentAndRssContainer">
-            <!--Importeren van de Navigatie balk (aan de functie wordt de huidige bestand naam mee gegeven-->
-            <?php navBar(basename($_SERVER['PHP_SELF'])) ?>
-            <div id="contentContainerFullWidth">
-                <div class="imgOrTextContainer">
-                    <div id="contactGrid">
-                        <div id="contactGridCell1">
-                            <img src="img/person_Icon.png" alt="Icoon van een persoon">
-                            <form method="POST">
-                                <input type="text" name="firstName" placeholder="Uw voornaam">
-                                <input type="text" name="lastName" placeholder="Uw achternaam">
-                        </div>
-                        <div id="contactGridCell2">
-                            <img src="img/email_Icon.png" alt="Icoon van een brief">
-                            <input type="email" name="email" class="email" placeholder="Uw emailadres">
-                        </div>
-                        <div id="contactGridCell3">
-                            <img src="img/phone2_Icon.png" alt="Icoon van een telefoon">
-                            <input type="number" name="tel" class="tel" placeholder="Uw telefoonnummer">
-                        </div>
-                        <div id="contactGridCell4">
-                            <img src="img/message_Icon.png" alt="Icoon van een bericht">
-                            <textarea name="message" placeholder="Bericht" class="textarea"></textarea>
-                            <input type="submit" name="submit" class="verzendKnop">
-                            </form>
-                        </div>
-                    </div>
-                </div>
-                <!--Importeren van de Footer-->
-                <?php footer() ?>
-            </div>
-        </div>
-    </body>
+        } else {
+            // Geeft een alert box weer met een waarschuwing
+            waarSchuwing();
+        }
+    }
+    ?>
+</div>
+</body>
 </html>
--- studiePage.php
+++ studiePage.php
@@ -1,115 +1,119 @@
 <!DOCTYPE html>
 <html>
-	<head>
-        <!--Bevat functions.php voor de lay-out, zorgt dat alle functies van functions.php hier gebruikt kan worden-->
-        <?php require_once "functions.php" ?>
-        <!--Importeer de basis informatie-->
-        <?php imports() ?>
-	</head>
-	<body>
-        <!--Importeren van de header-->
-        <?php headerBar() ?>
-        <div id="contentAndRssContainer">
-            <!--Importeren van de Navigatie balk (aan de functie wordt de huidige bestand naam mee gegeven-->
-            <?php navBar(basename($_SERVER['PHP_SELF'])) ?>
-			<div id="contentContainerFullWidth">
-				<div id="studiePageGrid">
-					<div id="gridCell1">
-						<h3>Werktuigbouwkunde</h3>
-					</div>
-					<div id="gridCell2">
-						<h3>Informatica</h3>
-					</div>
-					<div id="gridCell3">
-						<div class="imgOrTextContainer">
-							<h1>High tech system</h1>
-						</div>
-						<div class="imgOrTextContainer">
-							<h1>Composite design</h1>
-						</div>
-						<div class="imgOrTextContainer">
-							<h1>Sustainable polymers</h1>
-						</div>
-					</div>
-					<div id="gridCell4">
-						<div class="imgOrTextContainer">
-							<h1>Front end development</h1>
-						</div>
-						<div class="imgOrTextContainer">
-							<h1>Cloud</h1>
-						</div>
-						<div class="imgOrTextContainer">
-							<h1>Technical IT</h1>
-						</div>
-						<div class="imgOrTextContainer">
-							<h1>Software development</h1>
-						</div>
-					</div>
-				</div>
-				<div class="imgOrTextContainer">
-					<h1>Informatica</h1>
-				</div>
-				<div class="imgOrTextContainer">
-					<h1>Front end developer</h1>
-					<p>
-						Als front-end ontwikkelaar ben je verantwoordelijk voor de technische voorkant van een website, applicatie of programma. Jij zorgt ervoor dat het programma er niet
-						alleen goed uit ziet, maar ook echt  werkt.  Je houdt je bezig met het ontwerpen, programmeren, testen  en implementeren van het front-end gedeelte van een toepassing. 
-						Zowel op het gebied van de technische aspecten als op het gebied  van gebruiksmogelijkheid en toegankelijkheid.
-					</p>
-				</div>
-				<div class="imgOrTextContainer">
-					<h1>Cloud</h1>
-					<p>
-						Cloud computing is simpel gezegd het verhuizen van software en documenten van een computer naar het internet. Cloud computing is in de laatste jaren sterk gegroeid. 
-						Tegenwoordig zijn er dan ook steeds meer applicaties die alleen nog via het internet te bereiken zijn en niet gedownload kunnen worden.
-					</p>
-				</div>
-				<div class="imgOrTextContainer">
-					<h1>Technical IT</h1>
-					<p>
-						Als technisch informaticus ben je de uitvinder van moderne technische IT-systemen. 
-						Door jouw gecombineerde kennis van soft- en hardware ben je in staat om moderne, slimme, 
-						duurzame en veilige oplossingen te maken voor de steeds complexere uitdagingen van de toekomst. 
-						In het dagelijks leven zijn we steeds afhankelijker geworden van geautomatiseerde systemen.  
-					</p>
-				</div>
-				<div class="imgOrTextContainer">
-					<h1>Software development</h1>
-					<p>
-						Software ontwikkelen voor eindgebruikers in een professionele programmeertaal, het integreren van de cloud binnen de IT-werkzaamheden van je organisatie. 
-						Als software developer moet je slimme oplossingen kunnen verzinnen voor soms lastige vraagstukken. 
-						Ook moet je de drive hebben om je kennis up-to-date te houden.   
-					</p>
-				</div>
-				<div class="imgOrTextContainer">
-					<h1>Werktuigbouwkunde</h1>
-				</div>
-				<div class="imgOrTextContainer">
-					<h1>High Tech System</h1>
-					<p>
-						Binnen de opleiding High Tech Systems werk je met multidisciplinaire teams samen met bedrijven aan praktijkgericht onderzoek. 
-						In fysieke laboratoria met ultra moderne apparatuur. 
-						Aan deze opleiding zitten daarom redelijk hoge kosten. 
-					</p>
-				</div>
-				<div class="imgOrTextContainer">
-					<h1>Composite design</h1>
-					<p>
-						Als student van de composite design opleiding ga je bezig met technische elementen van hoge kwaliteit. 
-						De kerncompetentie van een samengesteld ontwerp ligt vooral in de hechting van verschillende high-tech materialen. 
-					</p>
-				</div>
-				<div class="imgOrTextContainer">
-					<h1>Sustainable polymers</h1>
-					<p>
-						Biologisch afbreekbare verpakkingen en frisdrankflessen gemaakt uit planten is realiteit geworden. 
-						Wil jij nieuwe biopolymeren ontwikkelen, duurzame prototypes 3D-printen of onderzoek doen naar de recyling van kunststofproducten? 
-						Dan is deze opleiding zeker iets voor jou!
-					</p>
-				</div>
-                <!--Importeren van de Footer-->
-                <?php footer() ?>
-			</div>
-		</div>
-	</body>
+<head>
+    <!--Bevat functions.php voor de lay-out, zorgt dat alle functies van functions.php hier gebruikt kan worden-->
+    <?php require_once "functions.php" ?>
+    <!--Importeer de basis informatie-->
+    <?php imports() ?>
+</head>
+<body>
+<!--Importeren van de header-->
+<?php headerBar() ?>
+<div id="contentAndRssContainer">
+    <!--Importeren van de Navigatie balk (aan de functie wordt de huidige bestand naam mee gegeven-->
+    <?php navBar(basename($_SERVER['PHP_SELF'])) ?>
+    <div id="contentAndRssContainer">
+        <div id="navBarWhithoutRss">
+            <ul>
+                <li id="usingPage"><a href="studiePage.html">Opleidingen</a></li>
+                <li><a href="photoPage.html">Foto's</a></li>
+                <li><a href="contactPage.html">Contact</a></li>
+            </ul>
+        </div>
+        <div id="contentContainerFullWidth">
+            <div id="studiePageGrid">
+                <div id="gridCell1">
+                    <h3>Werktuigbouwkunde</h3>
+                </div>
+                <div id="gridCell2">
+                    <h3>Informatica</h3>
+                </div>
+                <div id="gridCell3">
+                    <div class="imgOrTextContainer">
+                        <h1>High tech system</h1>
+                    </div>
+                    <div class="imgOrTextContainer">
+                        <h1>Composite design</h1>
+                    </div>
+                    <div class="imgOrTextContainer">
+                        <h1>Sustainable polymers</h1>
+                    </div>
+                </div>
+                <div id="gridCell4">
+                    <div class="imgOrTextContainer">
+                        <h1>Front end development</h1>
+                    </div>
+                    <div class="imgOrTextContainer">
+                        <h1>Cloud</h1>
+                    </div>
+                    <div class="imgOrTextContainer">
+                        <h1>Technical IT</h1>
+                    </div>
+                    <div class="imgOrTextContainer">
+                        <h1>Software development</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="imgOrTextContainer">
+                <h1>Informatica</h1>
+                <h1>Front end developer</h1>
+                <p>
+                    Als front-end ontwikkelaar ben je verantwoordelijk voor de technische voorkant van een website,
+                    applicatie of programma. Jij zorgt ervoor dat het programma er niet
+                    alleen goed uit ziet, maar ook echt werkt. Je houdt je bezig met het ontwerpen, programmeren, testen
+                    en implementeren van het front-end gedeelte van een toepassing.
+                    Zowel op het gebied van de technische aspecten als op het gebied van gebruiksmogelijkheid en
+                    toegankelijkheid.
+                </p>
+                <h1>Cloud</h1>
+                <p>
+                    Cloud computing is simpel gezegd het verhuizen van software en documenten van een computer naar het
+                    internet. Cloud computing is in de laatste jaren sterk gegroeid.
+                    Tegenwoordig zijn er dan ook steeds meer applicaties die alleen nog via het internet te bereiken
+                    zijn en niet gedownload kunnen worden.
+                </p>
+                <h1>Technical IT</h1>
+                <p>
+                    Als technisch informaticus ben je de uitvinder van moderne technische IT-systemen.
+                    Door jouw gecombineerde kennis van soft- en hardware ben je in staat om moderne, slimme,
+                    duurzame en veilige oplossingen te maken voor de steeds complexere uitdagingen van de toekomst.
+                    In het dagelijks leven zijn we steeds afhankelijker geworden van geautomatiseerde systemen.
+                </p>
+                <h1>Software development</h1>
+                <p>
+                    Software ontwikkelen voor eindgebruikers in een professionele programmeertaal, het integreren van de
+                    cloud binnen de IT-werkzaamheden van je organisatie.
+                    Als software developer moet je slimme oplossingen kunnen verzinnen voor soms lastige vraagstukken.
+                    Ook moet je de drive hebben om je kennis up-to-date te houden.
+                </p>
+            </div>
+            <div class="imgOrTextContainer">
+                <h1>Werktuigbouwkunde</h1>
+                <h1>High Tech System</h1>
+                <p>
+                    Binnen de opleiding High Tech Systems werk je met multidisciplinaire teams samen met bedrijven aan
+                    praktijkgericht onderzoek.
+                    In fysieke laboratoria met ultra moderne apparatuur.
+                    Aan deze opleiding zitten daarom redelijk hoge kosten.
+                </p>
+                <h1>Composite design</h1>
+                <p>
+                    Als student van de composite design opleiding ga je bezig met technische elementen van hoge
+                    kwaliteit.
+                    De kerncompetentie van een samengesteld ontwerp ligt vooral in de hechting van verschillende
+                    high-tech materialen.
+                </p>
+                <h1>Sustainable polymers</h1>
+                <p>
+                    Biologisch afbreekbare verpakkingen en frisdrankflessen gemaakt uit planten is realiteit geworden.
+                    Wil jij nieuwe biopolymeren ontwikkelen, duurzame prototypes 3D-printen of onderzoek doen naar de
+                    recyling van kunststofproducten?
+                    Dan is deze opleiding zeker iets voor jou!
+                </p>
+            </div>
+            <!--Importeren van de Footer-->
+            <?php footer() ?>
+        </div>
+    </div>
+</body>
 </html>
